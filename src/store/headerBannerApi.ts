26b371840643cae1c148f396bb5c08eb194ccce1
Nesat Nayem <nesatnayem042@gmail.com>
2025-10-23 11:18:53 +0600

    LOCAL URL

--- src/store/headerBannerApi.ts
+++ src/store/headerBannerApi.ts
@@ -17,7 +17,7 @@ export const headerBannerApi = createApi({
   reducerPath: "headerBanner",
   baseQuery: fetchBaseQuery({
     credentials: "include",
-    baseUrl: "https://api.atpuae.com/v1/api",
+    baseUrl: "http://localhost:8080/v1/api",
   }),
   tagTypes: ["headerBanner"],
   endpoints: (builder) => ({
